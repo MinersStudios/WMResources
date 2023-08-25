e40098f4a1c7fc94fdedad2402250587590c4dc9
Poloskun_ <nikongricisin339@gmail.com>
2023-08-25 16:11:43 +0300

    poopoogaga

--- zipper.py
+++ zipper.py
@@ -4,13 +4,13 @@ import time
 import zipfile
 import shutil
 
+start_time = time.time()
+total_files_processed = 0
+json_files_compressed = 0
+
 
 def compress_json(file_data):
     return json.dumps(file_data, separators=(",", ":"), ensure_ascii=False).encode("utf-8")
-
-
-total_files_processed = 0
-json_files_compressed = 0
 
 
 def process_file(path, destination_archive):
@@ -30,78 +30,85 @@ def process_file(path, destination_archive):
         destination_archive.write(path)
 
 
-start_time = time.time()
+def main():
+    pack_mcmeta_filename = "pack.mcmeta"
+    white_list_filename = "white_list.txt"
+    black_list_filename = "lite_black_list.txt"
+    full_archive_name = "FULL_WMTextures-v%s.zip"
+    lite_archive_name = "LITE_WMTextures-v%s.zip"
 
-pack_mcmeta_filename = "pack.mcmeta"
-white_list_filename = "white_list.txt"
-black_list_filename = "lite_black_list.txt"
-full_archive_name = "FULL_WMTextures-v%s.zip"
-lite_archive_name = "LITE_WMTextures-v%s.zip"
+    # Check if required files exist
 
-for required_file in [pack_mcmeta_filename, white_list_filename, black_list_filename]:
-    if not os.path.exists(required_file):
-        print("Missing file : " + required_file)
+    for required_file in [pack_mcmeta_filename, white_list_filename, black_list_filename]:
+        if not os.path.exists(required_file):
+            print("Missing file : " + required_file)
+            exit(1)
+
+    # Get pack version
+
+    with open(pack_mcmeta_filename, "r") as file:
+        pack_mcmeta = file.read()
+
+    if '"version": "' not in pack_mcmeta:
+        print("Missing \"version\" in " + pack_mcmeta_filename)
         exit(1)
 
-# Get pack version
+    pack_version = pack_mcmeta.split('"version": "')[1].split('"')[0]
+    full_archive_name = full_archive_name % pack_version
+    lite_archive_name = lite_archive_name % pack_version
 
-with open(pack_mcmeta_filename, "r") as file:
-    pack_mcmeta = file.read()
+    # Get white and black lists
 
-if '"version": "' not in pack_mcmeta:
-    print("Missing \"version\" in " + pack_mcmeta_filename)
-    exit(1)
+    with open(white_list_filename, "r") as file:
+        white_list = file.read().splitlines()
 
-pack_version = pack_mcmeta.split('"version": "')[1].split('"')[0]
-full_archive_name = full_archive_name % pack_version
-lite_archive_name = lite_archive_name % pack_version
+    with open(black_list_filename, "r") as file:
+        black_list = file.read().splitlines()
 
-# Get white and black lists
+    # Remove old archives
 
-with open(white_list_filename, "r") as file:
-    white_list = file.read().splitlines()
+    if os.path.exists(full_archive_name):
+        os.remove(full_archive_name)
 
-with open(black_list_filename, "r") as file:
-    black_list = file.read().splitlines()
+    if os.path.exists(lite_archive_name):
+        os.remove(lite_archive_name)
 
-# Remove old archives
+    # Create full archive with compressed JSON files
 
-if os.path.exists(full_archive_name):
-    os.remove(full_archive_name)
+    with zipfile.ZipFile(full_archive_name, "w", zipfile.ZIP_DEFLATED) as zip_file_full:
+        for white_path in white_list:
+            if white_path.endswith('/') or white_path.endswith('\\'):
+                for root, dirs, files in os.walk(white_path):
+                    for file_path in files:
+                        dest_path = os.path.join(root, file_path)
+                        process_file(dest_path, zip_file_full)
+            else:
+                process_file(white_path, zip_file_full)
 
-if os.path.exists(lite_archive_name):
-    os.remove(lite_archive_name)
+    # Create lite archive with compressed JSON files
 
-# Create full archive with compressed JSON files
+    with zipfile.ZipFile(full_archive_name, "r") as zip_file_full:
+        with zipfile.ZipFile(lite_archive_name, "w", zipfile.ZIP_DEFLATED) as zip_file_lite:
+            for file in zip_file_full.infolist():
+                if not any([file.filename.startswith(black_list_path) for black_list_path in black_list]):
+                    zip_file_lite.writestr(file, zip_file_full.read(file))
 
-with zipfile.ZipFile(full_archive_name, "w", zipfile.ZIP_DEFLATED) as zip_file_full:
-    for white_path in white_list:
-        if white_path.endswith('/') or white_path.endswith('\\'):
-            for root, dirs, files in os.walk(white_path):
-                for file_path in files:
-                    dest_path = os.path.join(root, file_path)
-                    process_file(dest_path, zip_file_full)
-        else:
-            process_file(white_path, zip_file_full)
+    # Print summary
 
-# Create lite archive with compressed JSON files
+    console_width = shutil.get_terminal_size().columns
+    decorative_line = "• ------------------------------ •".center(console_width)
 
-with zipfile.ZipFile(full_archive_name, "r") as zip_file_full:
-    with zipfile.ZipFile(lite_archive_name, "w", zipfile.ZIP_DEFLATED) as zip_file_lite:
-        for file in zip_file_full.infolist():
-            if not any([file.filename.startswith(black_list_path) for black_list_path in black_list]):
-                zip_file_lite.writestr(file, zip_file_full.read(file))
+    print(decorative_line)
+    print("Process completed successfully".center(console_width))
+    print(decorative_line)
+    print(f"Time taken: {round(time.time() - start_time, 2)} seconds".center(console_width))
+    print(f"Total files processed : {total_files_processed}".center(console_width))
+    print(f"JSON files compressed : {json_files_compressed}".center(console_width))
+    print("Size of the archives :".center(console_width))
+    print(f"- Full : {round(os.path.getsize(full_archive_name) / 1024, 2)} KB".center(console_width))
+    print(f"- Lite : {round(os.path.getsize(lite_archive_name) / 1024, 2)} KB".center(console_width))
+    print(decorative_line)
 
-console_width = shutil.get_terminal_size().columns
-decorative_line = "• ------------------------------ •".center(console_width)
 
-print(decorative_line)
-print("Process completed successfully".center(console_width))
-print(decorative_line)
-print(f"Time taken: {round(time.time() - start_time, 2)} seconds".center(console_width))
-print(f"Total files processed : {total_files_processed}".center(console_width))
-print(f"JSON files compressed : {json_files_compressed}".center(console_width))
-print("Size of the archives :".center(console_width))
-print(f"- Full : {round(os.path.getsize(full_archive_name) / 1024, 2)} KB".center(console_width))
-print(f"- Lite : {round(os.path.getsize(lite_archive_name) / 1024, 2)} KB".center(console_width))
-print(decorative_line)
+if __name__ == "__main__":
+    main()
